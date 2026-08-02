cf435cc6a30219cdfba8693df9b5a7d41fd6676c
Selim Altınkaynak <selim@hipmedya.com>
2026-08-02 16:32:14 +0300

    Attach bearer token to GraphQL requests

--- lib/apollo-wrapper.tsx
+++ lib/apollo-wrapper.tsx
@@ -9,32 +9,43 @@ import {
 } from "@apollo/client-integration-nextjs";
 import ErrorBoundary from "@/components/error/ErrorBoundary";
 import { createApolloErrorLink, GRAPHQL_ENDPOINT } from "@/lib/error-handler";
+import { getAccessToken } from "@/lib/auth";
 
 function makeClient() {
   const httpLink = new HttpLink({
     uri: GRAPHQL_ENDPOINT,
   });
 
+  const authLink = new ApolloLink((operation, forward) => {
+    const token = getAccessToken();
+
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+    }));
+
+    return forward(operation);
+  });
+
+  const transportLink = typeof window === "undefined"
+    ? ApolloLink.from([
+        new SSRMultipartLink({ stripDefer: true }),
+        httpLink,
+      ])
+    : ApolloLink.from([authLink, httpLink]);
+
   return new ApolloClient({
     cache: new InMemoryCache(),
     defaultOptions: {
-      query: {
-        errorPolicy: "all",
-      },
-      watchQuery: {
-        errorPolicy: "all",
-      },
+      query: { errorPolicy: "all" },
+      watchQuery: { errorPolicy: "all" },
     },
-    link:
-      typeof window === "undefined"
-        ? ApolloLink.from([
-            createApolloErrorLink("ssr"),
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
-        : ApolloLink.from([createApolloErrorLink("client"), httpLink]),
+    link: ApolloLink.from([
+      createApolloErrorLink(typeof window === "undefined" ? "ssr" : "client"),
+      transportLink,
+    ]),
   });
 }
 
