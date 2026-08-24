1df8a5996d409d07723ec411dd4fcd36b0a1be97
Selim Altınkaynak <selim@hipmedya.com>
2026-08-24 22:22:14 +0300

    Remove unused footer import

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ArrowRight, BriefcaseBusiness, Building2, CalendarDays, MapPinned, Newspaper } from "lucide-react";
+import { ArrowRight, BriefcaseBusiness, Building2, MapPinned, Newspaper } from "lucide-react";
 
 const linkGroups = [
   {
